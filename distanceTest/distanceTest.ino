abb593ee32af7a9e16645aae4500ae81611a9d46
Cassidy <cassidyjade623@gmail.com>
2019-04-08 11:08:48 -0400

    added testcase for defining distance

---
+++ distanceTest/distanceTest.ino
@@ -0,0 +1,348 @@
+// comments are not complete for subsystem funciton calls. They will be when I have extensively tested them when I am sure that I am satisfied with encoder behavior
+// encoder could stand to be optimized but given our previous complete lack of success we have given it a forgiving threshold.
+// results are as expected
+
+// Revisions to come
+
+
+#include <PID_v1.h>
+#include "CytronMotorDriver.h"
+#include <Stepper.h>
+
+
+#define MotEnable 9 //Motor Enamble pin Runs on PWM signal
+#define MotDir  6  // Motor Direction pin
+
+#define ENCODER0PINA         20      // this pin needs to support interrupts
+#define ENCODER0PINB         17      // this pin does not need to support interrupts
+#define CLOCKWISE            1       // direction constant
+#define COUNTER_CLOCKWISE    2       // direction constant
+#define REVOLUTIONS          10      // 30 = a distance of 2 inches
+#define CRPM                 30      // for carriage motor speed
+#define TRPM                 30      // for torque motor speed
+#define SRPM                 200     // for stepper motor speed
+#define DELAYMILLI           250     // set millisecond delay (used for torque and carriage)
+#define DELAYMCRO            10      // set microsecond delay (used for stepper)
+#define MAPval               90      // forgiving precision for encoder that maps 0 - 2000 to 0 - MAPval
+#define CPR                  2000    // Encoder Cycles per revolution.
+#define STOP                 0       
+#define SAMPLETIME           1       // set sample time to refresh PID in milliseconds
+// next is a control variable that is used as a bounds check for the position array
+short next = 0; 
+// scale each bolt location to allow a threshold greater than one degree precision 
+int bolt1 = map(1000,0, 2000, 0, MAPval);
+int bolt2 = map(1500,0, 2000, 0, MAPval);
+int bolt3 = map(500,0, 2000, 0, MAPval);
+int bolt4 = map(750,0, 2000, 0, MAPval);
+int bolt5 = map(1750,0, 2000, 0, MAPval);
+int bolt6 = map(250,0, 2000, 0, MAPval);
+int bolt7 = map(1250,0, 2000, 0, MAPval);
+
+// stuff those values into this array to iterate over
+int positions[8] = {bolt1,bolt2, bolt3, bolt4, bolt5, bolt6, bolt7, 0};
+
+// when all indices == 2 the COBOT has completed 3 passes. As of 4/04/19 this is unused but it shall be implemented in the next revision
+int pass[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+
+// variables modified by interrupt handler must be declared as volatile
+volatile long encoder0Position = 0;
+volatile long interruptsReceived = 0;
+ 
+// track last position so we know whether it's worth printing new output, used when debugging
+int previousPosition = 0;
+
+// logic control
+bool locationFound = false;
+bool keepLooking = true;
+
+// track direction: 0 = counter-clockwise; 1 = clockwise
+short currentDirection = CLOCKWISE;
+
+// Number of steps per output rotation // TODO: should change to a #define and all caps. 
+const int stepsPerRevolution = 200;
+
+// PID //TODO: explain constants
+double kp = .5 , ki = 1 , kd = .5; //kd prev vals: 1; 0.01;  kp prev vals: = 5, 1 KP = 2 BADD (WHEN KI = 1 AND KD = .5)
+// BEST SO FAR might be kp = 1, ki = 1, kd = .5
+// see details in ardiono PID library files 
+double input = 0, output = 0, setpoint = 0;
+// Create Instance of PID library
+PID myPID(&input, &output, &setpoint, kp, ki, kd, DIRECT); 
+
+// Configure the motor driver.
+// Create Instance of CytronMD library for torque motor
+CytronMD torqueMotor(PWM_DIR, 10, 7);  // PWM = Pin 6, DIR = Pin 7.
+// Create Instance of CytronMD library for carriage motor
+CytronMD carriageMotor(PWM_DIR, 9, 6);  // PWM = Pin 9, DIR = Pin 10.
+
+// Create Instance of Stepper library
+Stepper myStepper(stepsPerRevolution, 35, 37, 39, 41);
+
+void articulationSystemForward()
+{
+  
+  myStepper.setSpeed(SRPM);
+  Serial.println("Articulation System Activated yeeaaaaaa");
+  for(int i = 0; i < REVOLUTIONS; i++)
+  {
+  
+    myStepper.step(stepsPerRevolution);
+    delayMicroseconds(DELAYMCRO);
+  }
+
+activateTorqueMotor();
+}
+void articulationSystemBackward()
+{
+  Serial.println("/n Articulation System Activated /n");
+  
+  myStepper.setSpeed(SRPM);
+  for(int i = 0; i < REVOLUTIONS; i++)
+  {
+          myStepper.step(-stepsPerRevolution);
+          delayMicroseconds(DELAYMCRO);
+
+  }
+startCarriageMotor();
+
+}
+void activateTorqueMotor()
+{
+    //Serial.println("\n\n\n");
+  //Serial.println("Torque...");
+  //delay(1000);
+    torqueMotor.setSpeed(TRPM);
+    delay(1000);
+//  for(int go = 0; go < 5; go++)
+//  {
+//    
+//   // Serial.println("Torque...");
+//    delay(DELAYMILLI);
+//  }
+
+//
+ // Serial.println("Leaving. /n");
+    torqueMotor.setSpeed(STOP);
+    delay(DELAYMILLI);
+   // keepLooking = true;
+ //articulationSystemBackward();
+ startCarriageMotor();
+}
+void startCarriageMotor()
+ {
+//  Serial.println("\n\n\n");
+//  Serial.println("About to go look for location of next bolt...");
+//  
+//  carriageMotor.setSpeed(CRPM);
+//  delay(DELAYMILLI);
+//  Serial.println("Leaving.");
+  keepLooking = true;
+ }
+void setup() {
+  TCCR2B = TCCR2B & 0b11111000 | 0x01;  // set 31KHz PWM to prevent motor noise
+// dont remember why I commented this out. will look into later
+//  pinMode(ENCODER0PINA, INPUT);
+//  pinMode(ENCODER0PINB, INPUT);
+
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+//  pinMode(35, OUTPUT);
+//  pinMode(37, OUTPUT);
+//  pinMode(39, OUTPUT);
+//  pinMode(41, OUTPUT);
+  pinMode(MotEnable, OUTPUT);
+  pinMode(MotDir, OUTPUT); 
+
+  Serial.begin(9600); //initialize serial communication to enable diagnostic output
+
+  pinMode(ENCODER0PINA, INPUT_PULLUP); 
+  pinMode(ENCODER0PINB, INPUT_PULLUP);
+
+  digitalWrite(ENCODER0PINA, HIGH); 
+  digitalWrite(ENCODER0PINB, HIGH); 
+
+  attachInterrupt(3, updateEncoder, RISING); 
+    
+  myPID.SetMode(AUTOMATIC);   //set PID in Auto mode
+  myPID.SetSampleTime(SAMPLETIME);  // refresh rate of PID controller in milliseconds
+  myPID.SetOutputLimits(-CRPM, CRPM); //here change in value reflect change in speed limits of motor 
+
+  Serial.println("\n\n\n");
+  Serial.println("Ready.");
+
+ // final code will call articulation system. testing sometimes requires other behavior
+ // startCarriageMotor();
+ // articulationSystemBackward();
+ activateTorqueMotor();
+}
+
+void loop()
+{
+
+if (locationFound)
+  {
+
+//    Serial.println("FOUND LOCATION?");
+//    Serial.print(encoder0Position, DEC);
+//    Serial.print("\t");
+//    Serial.print(currentDirection == CLOCKWISE ? "clockwise" : "counter-clockwise");
+
+    // did we really find the location? If not let PID handle it, otherwise we got lucky so here we are 
+    // Dont' worry Reed there is no offset this is actual location compared to a location PID would find acceptable
+    if(interruptsReceived >= 9900 && interruptsReceived <=10000)
+    {
+       // reset location found  and disable other functions that are not desired when we are at a bolt location
+       locationFound = false;
+       keepLooking = false;
+       // debugging and data gathering 
+       Serial.println("YES! position that mapped was:  ");
+       Serial.println(encoder0Position);
+       Serial.println("cycles completed: ");
+       Serial.println(interruptsReceived,DEC);
+
+       // dont go out of bounds in our positions array! C be dangerous
+       if(next == 7)
+       {
+        next = 0;
+       }
+       else
+       {
+        next++;
+       }
+       
+       // debugging and data gathering 
+       Serial.println("bolt location that was found:  ");
+       Serial.println(next);
+       // final code will call articulation system, but for now we just need to do something other than sit here 
+       activateTorqueMotor();
+    }
+    else if(interruptsReceived > 10000)
+    {
+     Serial.print("We overshot: \t");
+
+       Serial.println("cycles completed: ");
+       Serial.println(interruptsReceived,DEC);
+//      Serial.print("actual value (that was mapped): \t");
+//      Serial.println(encoder0Position);
+    }
+  }
+   // debug stuff
+//  if (encoder0Position !=  previousPosition && keepLooking == true)
+//  {
+//
+////    Serial.println("Current Position: ");
+////    Serial.print(encoder0Position, DEC);
+////    Serial.print("\t");
+////    Serial.print(currentDirection == CLOCKWISE ? "clockwise" : "counter-clockwise");
+////    Serial.print("\t");
+////    Serial.println(interruptsReceived, DEC);
+//
+//      previousPosition = encoder0Position;
+//
+//  }
+
+   
+
+  // man I dont remember why this is here so I am leaving it! Ill figure it out later
+  input = encoder0Position;            
+
+  myPID.Compute();  // calculate new output
+  
+// only do this stuff if COBOT is not busy with other stuff
+if(keepLooking)
+{
+  setpoint = positions[next]; 
+    pwmOut(output);  
+}
+  
+  
+}
+void pwmOut(int out)
+{                               
+  if (out > 0)
+  { 
+   // Serial.println("setting motor speed to: ");
+    //Serial.println(out);   
+    analogWrite(MotEnable, out);         // Enabling motor enable pin to reach the desire angle
+    forward();                           // calling motor to move forward
+  }
+  else
+  {
+  //  Serial.println("setting motor speed to: ");
+   // Serial.println(out); 
+    analogWrite(MotEnable, abs(out));                        
+    reverse();                            // calling motor to move reverse
+  }
+  
+  
+
+}
+
+
+ 
+ 
+
+
+
+void updateEncoder()
+{
+// read both inputs
+  int a = digitalRead(ENCODER0PINA);
+  int b = digitalRead(ENCODER0PINB);
+ 
+  if (a == b )
+  {
+    // b is leading a (counter-clockwise)
+    encoder0Position--;
+    currentDirection = COUNTER_CLOCKWISE;
+  }
+  else
+  {
+    // a is leading b (clockwise)
+    encoder0Position++;
+    currentDirection = CLOCKWISE;
+  }
+ 
+  // track 0 to 1999
+  encoder0Position = encoder0Position % CPR;
+ // comment out for now
+// changing positions[next] to setspeed MAY improve accuracy. but I gotta think more. 
+//if(abs((map(encoder0Position, 0, 2000, 0, MAPval))) == positions[next])
+// {
+//  locationFound = true;
+//  // This stays
+//  carriageMotor.setSpeed(STOP);
+//  delay(DELAYMILLI);
+// }
+
+  interruptsReceived++;
+  if(interruptsReceived == 9900)
+  { 
+  locationFound = true;
+  // This stays
+  carriageMotor.setSpeed(STOP);
+  delay(DELAYMILLI);
+  }
+
+}
+
+void forward ()
+{
+  digitalWrite(MotDir, HIGH); 
+  
+}
+
+void reverse ()
+{
+  digitalWrite(MotDir, LOW); 
+ 
+  
+}
+// I dont even use this plus it doesn't do what I want it to. It stays for now
+void finish ()
+{
+  Serial.print("Stopping");
+  digitalWrite(MotDir, LOW); 
+ digitalWrite(MotEnable, LOW); 
+  
+}
